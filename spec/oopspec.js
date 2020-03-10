76dd86e96eb3dd96d1e3781a24f4109fe2c94eab
Vuyile <vuyile23@gmail.com>
2020-03-10 09:18:47 +0200

    first commit

--- spec/oopspec.js
+++ spec/oopspec.js
@@ -1,13 +1,36 @@
-let myTest = require ("../src/oopsrc")
+let myTest = require("../src/oopsrc");
 
-describe ("function 'sayHello' in a class of Person", function(){
-    let person = new myTest.Person("Ryan",
-    30,
-    "male",
-    ["being a hardarse", "agile", "ssd hard drives"]); 
-    let greeting = person.sayHello()
-it("should return greeting", function(){
-    expect(greeting).toEqual(`Hello, my name is Ryan and I am a 30 years old male, My interests are being a hardarse,agile,ssd hard drives`)
-})
+describe("function 'sayHello' in a class of Person", function() {
+  let person = new myTest.Person("Ryan", 30, "male", [
+    "being a hardarse",
+    "agile",
+    "ssd hard drives"
+  ]);
 
-})
+  let name = "Ryan";
+  it("should return name", () => {
+    expect(name).toEqual("Ryan");
+  });
+
+  let age = 30;
+  it("should return age", () => {
+    expect(age).toEqual(30);
+  });
+
+  let gender = "male";
+  it("should return the gender of Person", () => {
+    expect(gender).toEqual("male");
+  });
+
+  let interest = ["being a hardarse", "agile", "ssd hard drives"];
+  it("return interests of my Person", () => {
+    expect(interest).toEqual(["being a hardarse", "agile", "ssd hard drives"]);
+  });
+
+  let greeting = person.sayHello();
+  it("should return greeting", () => {
+    expect(greeting).toEqual(
+      `Hello, my name is Ryan and I am a 30 year old male, My interests are being a hardarse,agile,ssd hard drives`
+    );
+  });
+});
